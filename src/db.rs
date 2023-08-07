7f27252cad3cce3bce4717d87c2ee70b692d7099
DimitriTimoz <dimitri.timoz@orange.fr>
2023-08-07 23:05:05 +0200

    basic db

---
+++ src/db.rs
@@ -0,0 +1,22 @@
+use crate::prelude::*;
+
+pub struct Db<T> {
+    config: IxsiConfig,
+    /// The database containing all the tastes of the peers and interests points of the user.
+    my_tastes: T,
+    /// Filters received from seeders to find common points of interest.
+    seeder_filters: RwLock<BTreeMap<PeerId, ()>>,
+    /// Peers we send filters to find common points of interest.
+    leechers: RwLock<BTreeSet<PeerId>>,
+}
+
+impl<T> Db<T> {
+    pub fn new(config: IxsiConfig, my_tastes: T) -> Self {
+        Self {
+            config,
+            my_tastes,
+            leechers: RwLock::new(BTreeSet::new()),
+            seeder_filters: RwLock::new(BTreeMap::new()),
+        }
+    }
+}
